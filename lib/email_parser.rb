bce4f5d33ac9325abff86997889bd0e821581d11
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-26 04:50:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,8 +11,8 @@ class EmailAddressParser
   end
 
   def parse
-    @listemail.split(', ').uniq
-      #  @listemail.split.map {|i| i.split(',')}.flatten.uniq
+  #  @listemail.split(', ').uniq
+    @listemail.split.map {|i| i.split(',')}.flatten.uniq
   end
 
 end
